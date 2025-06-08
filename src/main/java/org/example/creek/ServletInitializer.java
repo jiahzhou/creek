ea35ba06e62106eeffa00694f0b42b43fa72f2d0
jiahaozhou <jiahaozhou@macmini.local>
2025-06-08 15:21:39 +0800

    reform

--- src/main/java/org/example/creek/ServletInitializer.java
+++ src/main/java/org/example/creek/ServletInitializer.java
@@ -1,14 +1,21 @@
 package org.example.creek;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
+@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(CreekApplication.class);
+        return application.sources(this.getClass());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletInitializer.class, args);
     }
 
 }
